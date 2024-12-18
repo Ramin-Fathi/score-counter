0f573ebce3d9f685015d33e712e9566c859db9ef
Ramin <56625817+Ramin-Fathi@users.noreply.github.com>
2024-12-18 15:41:05 +0330

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
 function selectTeams(numberOfTeams) {
-  console.log("Selected " + numberOfTeams + " teams");
+  // وقتی کاربر تعداد تیم رو انتخاب کرد میریم به teams.html و از طریق querystring تعداد تیم رو میفرستیم
+  window.location.href = `teams.html?teams=${numberOfTeams}`;
 }
